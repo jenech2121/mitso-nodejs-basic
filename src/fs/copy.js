79af29a7aebe55bcceb2c16f347659cb42c0d64e
Jenech WW <jenech.2121@gmail.com>
2026-03-02 22:57:34 +0300

    exactly like a create.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs';
 const copy = async () => {
     // Write your code here 
+    fs.mkdir('./files_copy', {recursive: true}, (err) =>{})
+    fs.cp('./files', './files_copy',{recursive:true}, (err) => {
+      if (err){;
+    console.log('FS operation success');
+    } else{
+        console.error('FS operation failed');
+    } 
+    });
 };
 
 await copy();
