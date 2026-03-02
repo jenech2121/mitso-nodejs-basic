189d7c0c37d481f0672d4a79553f397bd5ee63bc
Jenech WW <jenech.2121@gmail.com>
2026-03-02 22:15:48 +0300

    mb yes mb not ()

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs';
 const create = async () => {
     // Write your code here 
+    fs.writeFile('./files/fresh.txt', 'I am fresh and young', (err) => {
+    
+    if (err){;
+    console.log('FS operation success');
+    } else{
+        console.error('FS operation failed');
+    } 
+    });
 };
 
 await create();
