7b61c4f427087f00bf31e8e0662d95973de0c2f5
Jenech WW <jenech.2121@gmail.com>
2026-05-15 08:57:38 +0300

    calc comp

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises'
+import crypto from 'crypto'
+
 const calculateHash = async () => {
     // Write your code here 
+     try {
+        const data = await fs.readFile('./files/fileToCalculateHashFor.txt','utf-8');
+        console.log(data);
+        const hash = crypto.createHash('sha-256')
+        .update(data)
+        .digest('hex');
+
+        console.log(hash);
+    } catch(error) {
+        console.error("smth went wrong")
+    }
 };
 
 await calculateHash();
