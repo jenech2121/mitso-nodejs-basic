5e08e34f4785d6415ab9d93609792fbaa642c946
Jenech WW <jenech.2121@gmail.com>
2026-05-15 09:03:39 +0300

    cp dddo

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from "url";
+import { fork } from "child_process";
+import { cpus } from "os";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const scriptFiles = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+     fork(scriptFiles, args, {
+    stdio: [process.stdin, process.stdout, "ipc"],
+  });
 };
 
 // Put your arguments in function call to test this functionality
