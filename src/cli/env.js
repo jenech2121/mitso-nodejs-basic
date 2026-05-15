d954a948ccff033c71d6c87d8f886c85df7f1aac
Jenech WW <jenech.2121@gmail.com>
2026-05-15 08:30:49 +0300

    cli done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const result = Object.keys(process.env)
+    .filter((key) => key.startsWith("MITSO_"))
+    .map((key) => `${key}=${process.env[key]}`)
+    .join("; ");
+  console.log(result);
+
+  const all = Object.keys(process.env);
+  console.log(all); 
 };
 
 parseEnv();
