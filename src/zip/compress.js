d077891b7707819f73d1b127a5e3a000d468fee7
Jenech WW <jenech.2121@gmail.com>
2026-05-15 09:18:23 +0300

    zlib cooom

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import zlib from "zlib";
+import fs from "fs";
+import { pipeline } from "stream/promises";
+
 const compress = async () => {
     // Write your code here 
+     const readable = fs.createReadStream("./files/fileToCompress.txt");
+  const writable = fs.createWriteStream("./files/archive.gz");
+  const zip = zlib.createGzip();
+
+  try {
+    await pipeline(readable, zip, writable);
+    console.log("Завершено");
+  } catch (error) {
+    console.error("Smth went wrong");
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import zlib from "zlib";
+import fs from "fs";
+import { pipeline } from "stream/promises";
+
 const decompress = async () => {
     // Write your code here 
+  const readable = fs.createReadStream("./files/archive.gz");
+  const writable = fs.createWriteStream("./files/fileToCompress2.txt");
+  const zip = zlib.createGunzip();
+
+  try {
+    await pipeline(readable, zip, writable);
+    console.log("Завершено");
+  } catch (error) {
+    console.error("Smth went wrong");
+  }
 };
 
 await decompress();
